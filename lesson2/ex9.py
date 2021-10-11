bc53a76c05bbbed745eca2516c98d4e00f296e2d
lolicoder <91290970+lolicoder@users.noreply.github.com>
2021-10-11 14:30:31 +0300

    Update ex9.py

--- lesson2/ex9.py
+++ lesson2/ex9.py
@@ -1,1 +1,18 @@
+import turtle as tr 
 
+def circle(n,R):
+    for i in range(1,n+1):
+        tr.pendown()
+        tr.forward(R*3**0.5)
+        tr.left(360/n)
+    tr.right(90+180/n)
+    tr.penup()
+    tr.forward(15)
+    tr.left(90+180/(n+1))
+n = 10
+tr.shape('turtle')
+tr.speed(70)
+R = 30
+for i in range(3, n+1):
+    circle(i,R)
+    R+=15
